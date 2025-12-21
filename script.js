767a07eba68e04e6c1bc62a76c8606b06d7d859d
Deimox <gsnxofc@gmail.com>
2025-12-20 23:43:54 -0300

    api url changed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const pageSounds = document.querySelectorAll("#page_sfx");
 const xpSound = document.querySelector("#xp_sfx");
 const breakSound = document.querySelector("#break_sfx");
 
-const automodsApi = "https://targets-scanners-endorsed-womens.trycloudflare.com";
+const automodsApi = "https://personality-uniprotkb-invite-benjamin.trycloudflare.com";
 
 let mutedSounds = false;
 

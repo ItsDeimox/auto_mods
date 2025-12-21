dff7b5cf53d7aff6f10da8d20c45b5ed3d0d1e1c
Deimox <gsnxofc@gmail.com>
2025-12-21 15:49:54 -0300

    api url changed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const pageSounds = document.querySelectorAll("#page_sfx");
 const xpSound = document.querySelector("#xp_sfx");
 const breakSound = document.querySelector("#break_sfx");
 
-const automodsApi = "https://generation-omissions-reported-reno.trycloudflare.com";
+const automodsApi = "https://processors-light-cycles-potatoes.trycloudflare.com";
 
 let mutedSounds = false;
 

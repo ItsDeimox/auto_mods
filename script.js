35519f50e74ed8d9e59318c17c44553ad469ca35
Deimox <gsnxofc@gmail.com>
2025-12-24 12:58:46 -0300

    api url changed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const pageSounds = document.querySelectorAll("#page_sfx");
 const xpSound = document.querySelector("#xp_sfx");
 const breakSound = document.querySelector("#break_sfx");
 
-const automodsApi = "https://solomon-item-management-dis.trycloudflare.com";
+const automodsApi = "https://mirrors-eat-customized-straight.trycloudflare.com";
 
 let mutedSounds = false;
 

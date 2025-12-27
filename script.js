ce752ed93f13a6e20e70cac755748346514e236d
Deimox <gsnxofc@gmail.com>
2025-12-26 23:05:34 -0300

    url api updated

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const pageSounds = document.querySelectorAll("#page_sfx");
 const xpSound = document.querySelector("#xp_sfx");
 const breakSound = document.querySelector("#break_sfx");
 
-const automodsApi = "https://russia-protecting-computed-score.trycloudflare.com";
+const automodsApi = "https://sheffield-term-facing-bloom.trycloudflare.com";
 
 let mutedSounds = false;
 

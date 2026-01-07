6ca71ab0c3361d998400206d14ed74f0fe3ca34c
Deimox <gsnxofc@gmail.com>
2026-01-07 19:46:07 -0300

    api url updated

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const pageSounds = document.querySelectorAll("#page_sfx");
 const xpSound = document.querySelector("#xp_sfx");
 const breakSound = document.querySelector("#break_sfx");
 
-const automodsApi = "https://loc-freely-workout-wright.trycloudflare.com";
+const automodsApi = "https://entrance-towers-cheese-green.trycloudflare.com";
 
 let mutedSounds = false;
 

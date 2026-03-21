3a883237a603f3c2ae8d642469c03acab306788e
Deimox <gsnxofc@gmail.com>
2026-03-21 13:35:13 -0300

    url api changed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const pageSounds = document.querySelectorAll("#page_sfx");
 const xpSound = document.querySelector("#xp_sfx");
 const breakSound = document.querySelector("#break_sfx");
 
-const automodsApi = "https://spine-angeles-banana-vacancies.trycloudflare.com";
+const automodsApi = "https://reseller-atmosphere-org-lot.trycloudflare.com";
 
 let mutedSounds = false;
 
